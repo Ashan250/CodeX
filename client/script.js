4c4eab0b73fe60eee8dd9b8cfa3268dab39aa6c4
Janiya960 <ashan.janith250@gmail.com>
2023-01-07 14:32:01 +0530

    updated codex commit

--- client/script.js
+++ client/script.js
@@ -62,7 +62,7 @@ const handleSubmit = async(e)=>{
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-fmqy.onrender.com/', {
     method: 'POST',
     
     headers: {
